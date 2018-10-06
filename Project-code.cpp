ca0bdac1d0e1fb78bd2aaaa819acc96213c5de32
rogermiduba <40123901+rogermiduba@users.noreply.github.com>
2018-10-06 16:44:45 -0500

    Update Project-code.cpp

--- Project-code.cpp
+++ Project-code.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 void Input_data(Cantidades[3][3], Pesos[3][3])
 {
-    
+    float cantidadpolosjersey, cantidadpolospique,cantidadpolosfranela,cantidadcamisasjersey,cantidadcamisaspique,cantidadcamisasfranela,cantidadcuellosjersey, cantidadcuellospique,cantidadcuellosfranela,kgpolosjersey,kgpolospique,kgpolosfranela,kgcamisasjersey,kgcamisaspique,kgcamisasfranela,kgcuellosjersey, kgcuellospique,kgcuellosfranela;
+
+cout<< "Polos Jersey:";cin >> cantidadpolosjersey; cout << "kg/prenda:";cin >> kgpolosjersey;
+cout<< "Camisas Jersey:";cin >> cantidadcamisasjersey; cout << "Kg/prenda:";cin >> kgcamisasjersey;
+cout<< "Cuellos Jersey:";cin >> cantidadcuellosjersey; cout << "Kg/prenda";cin >> kgcuellosjersey;
+cout<< endl;
+cout<< "Polos Pique:";cin >> cantidadpolospique; cout << "Kg/prenda:";cin >> kgpolospique;
+cout<< "Camisas Pique:";cin >> cantidadcamisaspique; cout << "Kg/prenda:";cin >> kgcamisaspique;
+cout<< "Cuellos Pique:";cin >> cantidadcuellospique; cout << "Kg/prenda:";cin >> kgcuellospique;
+cout <<endl;
+cout<< "Polos Franela:";cin >> cantidadpolosfranela; cout << "Kg/prenda:";cin >> kgpolosfranela;
+cout<< "Camisas Franela:";cin >> cantidadcamisasfranela; cout << "Kg/prenda:";cin >> kgcamisasfranela;
+cout<< "Cuellos Franela:";cin >> cantidadcuellosfranela; cout << "Kg/prenda:";cin >> kgcuellosfranela;
+cout << endl;
 }
 
 int main()
