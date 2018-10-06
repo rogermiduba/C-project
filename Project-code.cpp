1d36d6848fc24f6c69fc0e7a1997524639cbd7e8
rogermiduba <40123901+rogermiduba@users.noreply.github.com>
2018-10-06 16:12:14 -0500

    Update Project-code.cpp

--- Project-code.cpp
+++ Project-code.cpp
@@ -1,2 +1,23 @@
 //Proyecto
 #include <iostream>
+using namespace std;
+int main()
+{
+    int Cantidades[3/*Telas*/][3/*Prendas*/] = {
+        {0,0,0},//Jersey
+        {0,0,0},//Pique
+        {0,0,0} //Franela
+//{Polos,Camisas,Cuellos}
+    }
+    
+    
+    float Pesos[3/*Telas*/][3/*Prendas*/] = {
+        {0,0,0},//Jersey
+        {0,0,0},//Pique
+        {0,0,0} //Franela
+//{Polos,Camisas,Cuellos}
+    }
+    
+    
+    return 0;
+}
